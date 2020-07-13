052c7483405ccb4ffa4b9918cece30239e482f4b
Rich Walton <sbtsun@yahoo.com>
2020-07-13 03:53:15 -0700

    new logic added almost, some refactoring, almost finished

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ GAME FUNCTION:
 let min = 1,
     max = 10,
     winningNum = 2,
-    guessesleft = 3;
+    guessesLeft = 3;
+    
 
 // UI elements --  
 
@@ -24,34 +25,46 @@ const game = document.querySelector('#game'),
 let count = 0;
 // Assign UI min max --   
 minNum.textContent = min;
-maxNum.textContent = max;   
-
+maxNum.textContent = max;
+ 
 // Listen for guess
 guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
-  count += 1;
-  if (count >= 3){
-    setMessage(`3 gueses are the limit start over`, 'red')
-    game.reset()
-    guessInput.value = '';
-  }
+  replayMes.textContent = '';
+  guessesLeft = guessesLeft-1;
   
-  console.log(count);
   // Validate -- could also use 'guess !== guess' instead of 'isNaN(guess)' to test for nothing eneterd
   if (isNaN(guess) || guess < min || guess > max){
     setMessage(`Please enter a number between ${min} and ${max}.`, 'red');
+  } else if(guess !== winningNum && guessesLeft === 0){
+    setMessage(`Sorry you lose, winning number is ${winningNum} press reset`, 'red');
+    startOver();
+    // guessesLeft = 3
   } else if(guess !== winningNum && guess < winningNum){
-    setMessage(`Too low`, 'black');
+    setMessage(`Too low: you have ${guessesLeft} more tries`, 'black');
   } else if(guess !== winningNum && guess > winningNum) {
-    setMessage(`Too high`, 'black');
+    setMessage(`Too high: you have ${guessesLeft} more tries`, 'black');
   } else if(guess === winningNum){
     setMessage(`YOU WIN!`, 'green');
-  }
+    startOver();
+    // guessesLeft = 3
+  } 
   guessInput.value = '';
 });
+
+let gamePara = document.querySelector('#game p');
+let replayMes = document.createElement('p');
+
+function startOver(){
+  guessInput.value = '';
+  guessesLeft = 3;
+  replayMes.textContent = `If you like to play again enter a new number`;
+  replayMes.style.color = 'green';
+  gamePara.appendChild(replayMes);
+  
+}
 
 function setMessage(msg, color) {
   message.style.color = color;
   message.textContent = msg;
 }
-
